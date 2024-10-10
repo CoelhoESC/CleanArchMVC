6b2eb749eb9704d3065ec58ec36129d83e02c91a
Everton Coelho <Everton Coelho@DESKTOP-LBHAPDB>
2024-10-10 20:17:05 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    internal class Category
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public ICollection<Product> products { get; set; } // um para muitos
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    internal class Product
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int stock {  get; set; }
+        public string image { get; set; }
+
+        public int categoryId { get; set; } // foreign-key
+        public Category category { get; set; }
+    }
+}
