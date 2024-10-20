d652f7c722ac27b907dc1fad8dbe4a15d755eb9b
Everton Coelho <Everton Coelho@DESKTOP-LBHAPDB>
2024-10-20 15:28:55 -0300

    Configurando as entidades usando Fluent API

---
+++ CleanArchMVC.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(t => t.id);
+            builder.Property(t => t.name).HasMaxLength(100).IsRequired();
+            builder.Property(t => t.description).HasMaxLength(200).IsRequired();
+            builder.Property(t => t.price).HasPrecision(10,2);
+            builder.HasOne(t => t.category).WithMany(t => t.products).HasForeignKey(t => t.categoryId);
+        }
+    }
+}
