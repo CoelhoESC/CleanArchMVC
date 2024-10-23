e78429de89254db58d20e89c689343316e219cbf
Everton Coelho <Everton Coelho@DESKTOP-LBHAPDB>
2024-10-22 21:19:26 -0300

    Fazendo o CRUD para Produtos

--- CleanArchMVC.Infra.Data/Repositories/ProductRepository.cs
+++ CleanArchMVC.Infra.Data/Repositories/ProductRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,53 @@ using System.Threading.Tasks;
 
 namespace CleanArchMVC.Infra.Data.Repositories
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        readonly ApplicationDbContext dbcontext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            dbcontext = context;
+        }
+
+        public async Task<Product> CreateProduct(Product product)
+        {
+            dbcontext.Add(product);
+            await dbcontext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetByID(int? id)
+        {
+            return await dbcontext.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProduct()
+        {
+            return await dbcontext.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductCategory(int? id)
+        {
+            return await dbcontext.Products.Include(c => c.category).SingleOrDefaultAsync(p => p.id == id); // carregamento adiantado
+        }
+
+        public async Task<Product> RemmoveProduct(Product product)
+        {
+            dbcontext.Remove(product);
+            await dbcontext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateProduct(Product product)
+        {
+            dbcontext.Update(product);
+            await dbcontext.SaveChangesAsync();
+            return product;
+        }
     }
 }
